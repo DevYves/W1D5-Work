d22f7e5b051120e51f7d277678a2e93f1af85569
DevYves <yfreypons@gmail.com>
2017-11-03 18:08:30 +0000

    first private punction

---
+++ module.js
@@ -0,0 +1,14 @@
+
+/** returns a number which is the sum of the numbers passed.
+*
+*
+*@param {array<number>} numbers - the number t sum
+*@return {number} the sum of the numnbers
+*/
+
+
+
+function sum(sum){
+  return 0;
+}
+
